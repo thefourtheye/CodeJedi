30038aa12826af81fac69ee1c944dba73f5be384
thefourtheye <thechargingvolcano@gmail.com>
2014-12-31 18:37:37 +0000

    Scratching the current work

--- common.py
+++
@@ -1,8 +0,0 @@
-import web, sys, os, logging
-templates = os.path.join(os.path.dirname(__file__) + "/templates/")
-sys.path.insert(1, templates)
-Templates = web.template.render (templates, base='master')
-from web import ctx as Context
-from dynamic.core.genericmodel  import GenericModel as Object
-from dynamic.core.mongodb       import MongoDb      as MongoDB
-Logger = logging.getLogger ('')
--- urls.py
+++
@@ -1,9 +0,0 @@
-url_mappings = (
-	'/', 'index.index',
-	'/authenticate', 'dynamic.authentication.authentication.Authenticate',
-	'/problems/submit', 'dynamic.problems.SubmitProblems.SubmitProblems',
-	'/home', 'index.Home',
-	'/logout', 'dynamic.authentication.logout.UserLogout',
-	'/invalidsession', 'dynamic.authentication.logout.InvalidSession',
-	'/expiredsession', 'dynamic.authentication.logout.ExpiredSession',
-)
