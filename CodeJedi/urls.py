5d13071572fac4916721fd7da31bb4d6e7488cf6
thefourtheye <thechargingvolcano@gmail.com>
2015-02-21 19:48:29 +0000

    Initial Commit with Django1.7 and problems app

---
+++ CodeJedi/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^blog/', include('blog.urls')),
+    # url(r'^$', 'CodeJedi.views.home', name='home'),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
